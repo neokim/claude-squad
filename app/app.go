0881e592e67d9ae554b9ccb13dfaa6da6a7f93a1
Neo Kim <one@neo.kim>
2026-04-21 17:46:21 +0900

    feat: add Alt+Backspace and Ctrl+U for title input
    Alt+Backspace deletes the last word, Ctrl+U clears the entire title.

--- app/app.go
+++ app/app.go
@@ -446,11 +446,36 @@ func (m *home) handleKeyPress(msg tea.KeyMsg) (mod tea.Model, cmd tea.Cmd) {
 				return m, m.handleError(err)
 			}
 		case tea.KeyBackspace:
-			runes := []rune(instance.Title)
-			if len(runes) == 0 {
-				return m, nil
+			if msg.Alt {
+				// Alt+Backspace: delete last word
+				runes := []rune(instance.Title)
+				if len(runes) == 0 {
+					return m, nil
+				}
+				// Skip trailing spaces
+				i := len(runes) - 1
+				for i >= 0 && runes[i] == ' ' {
+					i--
+				}
+				// Skip word characters
+				for i >= 0 && runes[i] != ' ' {
+					i--
+				}
+				if err := instance.SetTitle(string(runes[:i+1])); err != nil {
+					return m, m.handleError(err)
+				}
+			} else {
+				runes := []rune(instance.Title)
+				if len(runes) == 0 {
+					return m, nil
+				}
+				if err := instance.SetTitle(string(runes[:len(runes)-1])); err != nil {
+					return m, m.handleError(err)
+				}
 			}
-			if err := instance.SetTitle(string(runes[:len(runes)-1])); err != nil {
+		case tea.KeyCtrlU:
+			// Ctrl+U: clear entire title
+			if err := instance.SetTitle(""); err != nil {
 				return m, m.handleError(err)
 			}
 		case tea.KeySpace:
